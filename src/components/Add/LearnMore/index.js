9b0ccdb1f85af1e4d1b8366949a4df7b199f8831
webot-solve <webotsolve@gmail.com>
2021-09-21 09:34:32 -0700

    Added Input: LearnMore, Custom Fields, Description, Locations and Images

--- src/components/Add/LearnMore/index.js
+++ src/components/Add/LearnMore/index.js
@@ -1,8 +1,78 @@
 import React from 'react'
 
-export default function AddLearnMore(){
+import DashHeader from "../../DashHeader";
+import TextPickerCtrl from "../../../controllers/Forms/TextPicker/TextPickerCtrl";
+import MediaPickerCtrl from "../../../controllers/Forms/MediaPicker/MediaPickerCtrl";
+import CustomFieldPickerCtrl from "../../../controllers/Forms/CustomFieldPicker/CustomFieldPickerCtrl";
+import TextInputCtrl from "../../../controllers/Forms/TextInput/TextInputCtrl";
+import TextAreaCtrl from "../../../controllers/Forms/TextArea/TextAreaCtrl";
+import TogglerCtrl from "../../../controllers/Forms/Toggler/TogglerCtrl";
+import Message from "../../Message";
+
+export default function AddLearnMore({
+  // METHODS
+  locationsChanged,
+  imagesChanged,
+
+  learnMoreNameChanged,
+  customFieldsChanged,
+  descriptionChanged,
+
+  // SELECTION DATA
+  eLocations,
+  eImages,
+
+  // QUERIES
+  queryLocations,
+  queryImages,
+}){
 
   return (
-  <> hello from component Add Learn More</>
+    <div>
+      <DashHeader
+        title="Add New Learn More"
+        action="Publish"
+        // method={handlePublish}
+        // loading={loading}
+      />
+      <br></br>
+      <div className="form__grid">
+        <div className="col">
+          <TextInputCtrl
+              label={"Learn More"}
+              setter={(data) => learnMoreNameChanged(data)}
+            />      
+          <CustomFieldPickerCtrl
+            label={"Custom Field"}
+            setter={(data) => customFieldsChanged(data)}
+          />
+          <TextAreaCtrl
+            label={"Description"}
+            setter={(data) => descriptionChanged(data)}
+          />
+          <TextPickerCtrl
+            label={"location"}
+            dataLabel={"location"}
+            data={eLocations}
+            query={queryLocations}
+            setter={(data) => locationsChanged(data)}
+          />
+        </div>
+        <div className="col">
+          <MediaPickerCtrl
+            label={"image"}
+            dataLabel={"image"}
+            data={eImages}
+            query={queryImages}
+            setter={(data) => imagesChanged(data)}
+          />
+          
+        </div>
+
+
+      </div>
+      
+
+    </div>
   )
 }
--- src/controllers/Add/LearnMore/AddLearnMoreCtrl.js
+++ src/controllers/Add/LearnMore/AddLearnMoreCtrl.js
@@ -1,9 +1,98 @@
 import React, {useState, useEffect} from 'react';
 import AddLearnMore from '../../../components/Add/LearnMore';
 
+import {
+  getLocations,
+  getImages,
+ 
+} from "../../../network";
+
+
 export default function AddLearnMoreCtrl(){
 
+  let isMounted = true;
+
+  // ===============================================================
+  // FORM DATA
+  // @desc form control data
+  // ===============================================================
+
+  const [locations, setLocations] = useState([]);
+  const [images, setImages] = useState([]);
+  const [customFields, setCustomFields] = useState([]);
+  const [learnMore, setLearnMoreName] = useState("");
+  const [description, setDescription] = useState("");
+
+    // ===============================================================
+  // SELECTION DATA
+  // @desc data that appears as options in select boxes.
+  // ===============================================================
+  const [eLocations, setELocations] = useState([]);
+  const [eImages, setEImages] = useState([]);
+
+// ===============================================================
+  // NETWORK QUERIES FOR EXISTING DATA
+  // @desc queries for existing data
+  // ===============================================================
+  const queryLocations = async () => {
+    const result = await getLocations();
+    if (result.error) return;
+    if (!isMounted) return;
+    setELocations(result);
+  };
+
+  const queryImages = async () => {
+    const result = await getImages();
+    if (result.error) return;
+    if (!isMounted) return;
+    setEImages(result);
+  };
+
+   // ===============================================================
+  // INPUT WATCHERS AND SETTERS
+  // @desc functions that watch updates in children components, and sets them here.
+  // ===============================================================
+
+
+  const imagesChanged = (data) => {
+    const mappedData = data.map((d) => d._id);
+    setImages(mappedData);
+  };
+  const learnMoreNameChanged = (data) => {
+    setLearnMoreName(data);
+  };
+  const descriptionChanged = (data) => {
+    setDescription(data);
+  };
+  const customFieldsChanged = (data) => {
+    setCustomFields(data);
+  };
+
+    // ===============================================================
+  // INPUT WATCHERS AND SETTERS
+  // @desc functions that watch updates in children components, and sets them here.
+  // ===============================================================
+
+  const locationsChanged = (data) => {
+    const mappedData = data.map((d) => d._id);
+    setLocations(mappedData);
+  };
+
   return (
-    <AddLearnMore/>
+    <AddLearnMore
+
+      locationsChanged={locationsChanged}
+      imagesChanged={imagesChanged}
+
+      customFieldsChanged={customFieldsChanged}
+      learnMoreNameChanged={learnMoreNameChanged}
+      descriptionChanged={descriptionChanged}
+      
+      eLocations={eLocations}
+      eImages={eImages}
+      queryImages={queryImages}
+
+      queryLocations={queryLocations}
+    />
   );
 }
