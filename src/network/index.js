4ca9f9ef096c3cf51dc9def0b710ec0bd84135a2
webot-solve <webotsolve@gmail.com>
2021-09-23 10:37:30 -0700

    Added AddTour: Network Query createTour

--- src/network/index.js
+++ src/network/index.js
@@ -1395,3 +1395,28 @@ export const bulkDeleteTours = async (array) => {
     };
   }
 };
+
+export const createTour = async (tour) => {
+  const token = getToken();
+
+  if (!token)
+    return {
+      error: "No token found. Could not authenticate request.",
+    };
+
+  try {
+    const response = await axios.post(`${BASE_URL}/tours`, tour, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error.response);
+    return {
+      error: error.response,
+    };
+  }
+};
